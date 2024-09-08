0790d738a9b1712861bb983fbe344f88a392a04d
idoreu <103368518+idoreu@users.noreply.github.com>
2024-09-08 22:13:40 +0300

    Update ProfileDisplay.tsx

--- src/components/ProfileDisplay.tsx
+++ src/components/ProfileDisplay.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import { UserData } from './utilities';
+import React, { useContext } from 'react';
+import { userContext } from './utilities';
 
-interface ProfileDisplayProps {
-    userData: UserData;
-    userImage: string| null;
-}
+function ProfileDisplay() {
+    const userCntxt = useContext(userContext);
 
-const ProfileDisplay: React.FC<ProfileDisplayProps> = ({userData, userImage}) => {
+    const {user, setUser} = userCntxt;
+
     return (
-        <div>
-            <h2>Profile Information</h2>
-            {userImage && <img src={userImage} alt="User profile" style={{ width: '100px', height: '100px', borderRadius: '50%' }} />}
-            <p>Name: {userData.name}</p>
-            <p>Age: {userData.age}</p>
-            <p>Email: {userData.email}</p>
-            <p>Address: {userData.address}</p>
-            <p>State: {userData.state}</p>
-            <p>Phone: {userData.phone}</p>
+        <div className='container mt-5'>
+            <h2 className='container mb-4'>Profile Information</h2>
+            <div className='card' style={{ maxWidth: '18rem' }}>
+                {user.Image && (
+                    <img
+                    src={user.Image}
+                    className='card-img-top'
+                    alt="User profile"
+                    style={{ width: '100px', height: '100px', borderRadius: '15%' }}
+                    />
+                )}
+            </div>
+            <div className="card-body">
+                <h5 className="card-title">{user.name}</h5>
+                <p className="card-text">
+                    <strong>Age:</strong> {user.age}<br />
+                    <strong>Email:</strong> {user.email}<br />
+                    <strong>Address:</strong> {user.address}<br />
+                    <strong>State:</strong> {user.state}<br />
+                    <strong>Phone:</strong> {user.phone}
+                </p>
+            </div>
         </div>
     );
 };
